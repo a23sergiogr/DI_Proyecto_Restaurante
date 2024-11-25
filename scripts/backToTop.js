3530abe72c410ad4501858ff4cd34e905cfa9554
dam2 - Gómez Rial <A23SergioGR@sanclemente.local>
2024-11-25 14:23:46 +0100

    update main

--- scripts/backToTop.js
+++ scripts/backToTop.js
@@ -1,10 +1,20 @@
 window.addEventListener('scroll', function () {
     const backToTopButton = document.getElementById('backToTop');
+    const timeline = document.getElementById('timeline');
 
-    if (window.scrollY > 11500) {
+    
+    var windowHeight = window.innerHeight;
+
+    if (window.scrollY > 10000) {
         backToTopButton.classList.add('show'); // Muestra el botón
     } else {
         backToTopButton.classList.remove('show'); // Oculta el botón
+    }
+
+    if (window.scrollY > 800 && this.window.scrollY < 10000) {
+        timeline.classList.add('show'); // Muestra la Timeline
+    } else {
+        timeline.classList.remove('show'); // Muestra la Timeline
     }
 });
 
