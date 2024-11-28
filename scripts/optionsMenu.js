81558601f254066419bfb7617d72122d7fe1a097
a23sergiogr <147613072+a23sergiogr@users.noreply.github.com>
2024-11-28 13:07:48 +0100

    add book.html and book.css

---
+++ scripts/optionsMenu.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const guestsSelect = new Choices('#guests', {
+        searchEnabled: false, 
+        itemSelectText: '',  
+        shouldSort: false,  
+    });
+});
