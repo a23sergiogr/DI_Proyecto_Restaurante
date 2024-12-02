791e37b5caaa2b8fe5293b457ab2f49233688075
dam2 - Gómez Rial <A23SergioGR@sanclemente.local>
2024-12-02 14:04:26 +0100

    update barsMenu

--- scripts/barsMenu.js
+++ scripts/barsMenu.js
@@ -1,7 +1,9 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const barContainer = document.querySelector('.bar-container');
     const dropdownMenu = document.querySelector('.bar-container .dropdown');
     const menuOptions = document.querySelectorAll('.menu-option');
+    const flags = document.querySelectorAll('.flagLenguage');
 
     barContainer.addEventListener('click', () => {
                 dropdownMenu.classList.toggle('hidden');
@@ -18,4 +20,27 @@ document.addEventListener('DOMContentLoaded', () => {
             dropdownMenu.style.flexDirection = 'column';
         }
     });
+
+    flags.forEach(flag => {
+        flag.addEventListener('click', (event) => {
+            event.stopPropagation();
+
+            // Desactiva todas las banderas
+            flags.forEach(f => {
+                f.classList.remove('active', 'custom-colors');
+                f.classList.add('inactive');
+            });
+
+            // Activa la bandera seleccionada y aplica el filtro
+            flag.classList.remove('inactive');
+            flag.classList.add('active', 'custom-colors');
+        });
+    });
+
+    // Opcional: Establece una bandera como activa por defecto
+    if (flags.length > 0) {
+        flags[0].classList.remove('inactive');
+        flags[0].classList.add('active', 'custom-colors');
+    }
 });
+
